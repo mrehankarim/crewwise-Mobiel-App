f04c05c28c2178fb510823ba8163187ca3cc3ebb
Muhammad Rehan <162461517+mrehankarim@users.noreply.github.com>
2026-04-19 21:47:01 +0500

    Created Profile Page

---
+++ app/src/main/java/com/example/crewwise/WorkOrderDetailsFragment.java
@@ -0,0 +1,33 @@
+package com.example.crewwise;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class WorkOrderDetailsFragment extends Fragment {
+
+
+
+
+    public WorkOrderDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_work_order_details, container, false);
+    }
+}
